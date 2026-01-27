02db5c6eb393e927bce4e7b50785273a31d88ef6
bayezid-hossain <arkantos.tas@gmail.com>
2026-01-27 21:27:28 +0600

    added suspense boundary for verify-email

--- app/(auth)/verify-email/page.tsx
+++ app/(auth)/verify-email/page.tsx
@@ -1,5 +1,15 @@
 import { VerifyEmailView } from "@/modules/auth/ui/views/verify-email-view";
+import { Loader2 } from "lucide-react";
+import { Suspense } from "react";
 
 export default function VerifyEmailPage() {
-    return <VerifyEmailView />;
+    return (
+        <Suspense fallback={
+            <div className="flex h-screen w-full items-center justify-center">
+                <Loader2 className="h-8 w-8 animate-spin text-primary" />
+            </div>
+        }>
+            <VerifyEmailView />
+        </Suspense>
+    );
 }
